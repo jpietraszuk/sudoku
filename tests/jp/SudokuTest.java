59c83f2de863915f54f855b714eb10000b2bc308
JP <jpietraszuk@gmail.com>
2016-10-15 12:16:52 +0100

    Initial version of application

---
+++ tests/jp/SudokuTest.java
@@ -0,0 +1,11 @@
+package jp;
+
+import org.junit.Test;
+
+public class SudokuTest {
+
+    @Test
+    public void check() {
+        Sudoku sudoku = new Sudoku("040000000001034620603000070000483507000050060000009040005000001800547396000021000");
+    }
+}
